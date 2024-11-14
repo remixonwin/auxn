32d7b35f8cb6244a7aeef3d142029ad0b1f1ab86
remixonwin <remixonwin@gmail.com>
2024-11-14 03:57:39 +0000

    heelo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "hello"
 )
